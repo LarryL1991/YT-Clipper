d898d5a59ec92d2b92854c31388362942ff45423
unknown <=>
2023-02-28 21:05:01 -0600

    initial upload to github
    description

---
+++ YT Clipper/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace YT_Clipper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lengthBox = new System.Windows.Forms.TextBox();
+            this.endBox = new System.Windows.Forms.TextBox();
+            this.startBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.fileButton = new System.Windows.Forms.Button();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.directoryBox = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.CMDBox = new System.Windows.Forms.RichTextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Link";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.lengthBox);
+            this.groupBox1.Controls.Add(this.endBox);
+            this.groupBox1.Controls.Add(this.startBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.linkBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(438, 180);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(323, 98);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 20);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "from start";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(333, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Length";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(279, 123);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "OR";
+            // 
+            // lengthBox
+            // 
+            this.lengthBox.Location = new System.Drawing.Point(323, 121);
+            this.lengthBox.Name = "lengthBox";
+            this.lengthBox.Size = new System.Drawing.Size(78, 27);
+            this.lengthBox.TabIndex = 6;
+            this.lengthBox.Enter += new System.EventHandler(this.lengthBox_Enter);
+            this.lengthBox.Leave += new System.EventHandler(this.lengthBox_Leave);
+            // 
+            // endBox
+            // 
+            this.endBox.Location = new System.Drawing.Point(167, 121);
+            this.endBox.Name = "endBox";
+            this.endBox.Size = new System.Drawing.Size(99, 27);
+            this.endBox.TabIndex = 5;
+            this.endBox.Enter += new System.EventHandler(this.endBox_Enter);
+            this.endBox.Leave += new System.EventHandler(this.endBox_Leave);
+            // 
+            // startBox
+            // 
+            this.startBox.Location = new System.Drawing.Point(17, 121);
+            this.startBox.Name = "startBox";
+            this.startBox.Size = new System.Drawing.Size(99, 27);
+            this.startBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(167, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "End Time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Start Time";
+            // 
+            // linkBox
+            // 
+            this.linkBox.Location = new System.Drawing.Point(13, 46);
+            this.linkBox.Name = "linkBox";
+            this.linkBox.Size = new System.Drawing.Size(384, 27);
+            this.linkBox.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.fileButton);
+            this.groupBox2.Controls.Add(this.nameBox);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.directoryBox);
+            this.groupBox2.Location = new System.Drawing.Point(12, 198);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(438, 174);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            // 
+            // fileButton
+            // 
+            this.fileButton.Location = new System.Drawing.Point(393, 55);
+            this.fileButton.Name = "fileButton";
+            this.fileButton.Size = new System.Drawing.Size(32, 29);
+            this.fileButton.TabIndex = 4;
+            this.fileButton.Text = "...";
+            this.fileButton.UseVisualStyleBackColor = true;
+            this.fileButton.Click += new System.EventHandler(this.fileButton_Click);
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(17, 119);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(185, 27);
+            this.nameBox.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(272, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Save as... (File Name) (Blank for default)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(194, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Directory (Blank for default)";
+            // 
+            // directoryBox
+            // 
+            this.directoryBox.Location = new System.Drawing.Point(17, 56);
+            this.directoryBox.Name = "directoryBox";
+            this.directoryBox.Size = new System.Drawing.Size(370, 27);
+            this.directoryBox.TabIndex = 0;
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(507, 482);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(155, 45);
+            this.goButton.TabIndex = 3;
+            this.goButton.Text = "Go!";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // CMDBox
+            // 
+            this.CMDBox.Location = new System.Drawing.Point(12, 382);
+            this.CMDBox.Name = "CMDBox";
+            this.CMDBox.Size = new System.Drawing.Size(650, 94);
+            this.CMDBox.TabIndex = 4;
+            this.CMDBox.Text = "";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(14, 479);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 20);
+            this.statusLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 533);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.CMDBox);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "YouTube Clipper";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox linkBox;
+        private System.Windows.Forms.TextBox startBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox directoryBox;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox lengthBox;
+        private System.Windows.Forms.TextBox endBox;
+        private System.Windows.Forms.RichTextBox CMDBox;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button fileButton;
+    }
+}
+
---
+++ YT Clipper/Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace YT_Clipper
+{
+    public partial class Form1 : Form
+    {
+        private string DL_Vid_Name;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void endBox_Leave(object sender, System.EventArgs e)
+        {
+            if (endBox.Text != "")
+            {
+                lengthBox.ReadOnly = true;
+                lengthBox.BackColor = Color.LightGray;
+                lengthBox.TabStop = false;
+            }
+            else
+            {
+                lengthBox.ReadOnly = false;
+                lengthBox.BackColor = Color.White;
+                lengthBox.TabStop = true;
+            }
+            
+        }
+
+        private void endBox_Enter(object sender, System.EventArgs e)
+        {
+            if (endBox.ReadOnly == true)
+            {
+                lengthBox.Focus();
+            }
+        }
+
+        private void lengthBox_Leave(object sender, System.EventArgs e)
+        {
+            if (lengthBox.Text != "")
+            {
+                endBox.ReadOnly = true;
+                endBox.BackColor = Color.LightGray;
+                endBox.TabStop = false;
+            }
+            else
+            {
+                endBox.ReadOnly = false;
+                endBox.BackColor = Color.White;
+                endBox.TabStop = true;
+            }
+        }
+
+        private void lengthBox_Enter(object sender, System.EventArgs e)
+        {
+            if(lengthBox.ReadOnly == true)
+            {
+                directoryBox.Focus();
+            }
+        }
+
+        private int readTime(string timeString)
+        {
+            bool succeeded;
+            int sIndex  = 0;
+            int sIndex2 = 0;
+            int seconds;
+            int minutes;
+            int hours;
+
+            //string source = timeString;
+            if (String.IsNullOrEmpty(timeString))
+                return 0;
+            
+            //count how many colons are in the start/end box
+            int colonCount = 0;
+            foreach (char c in timeString)
+                if (c == ':') colonCount++;
+
+            switch(colonCount)
+            {
+                case 0:
+                    succeeded = Int32.TryParse(timeString, out seconds);
+                    if (succeeded)
+                        return seconds; //no colons and time successfully parsed.
+                    else
+                        return -1;
+                case 1:
+                    sIndex = timeString.IndexOf(":", 0);
+                    succeeded = Int32.TryParse(timeString.Substring(0, sIndex), out minutes);
+                    if (succeeded)
+                    {
+                        succeeded = Int32.TryParse(timeString.Substring(sIndex + 1), out seconds);
+                        if (succeeded)
+                            return (minutes * 60) + seconds;
+
+                    }
+                    return -1;
+                case 2:
+                    sIndex = timeString.IndexOf(":", 0);
+                    sIndex2 = timeString.IndexOf(":", sIndex + 1);
+                    succeeded = Int32.TryParse(timeString.Substring(0, sIndex), out hours);
+                    if(succeeded)
+                    {
+                       
+                        succeeded = Int32.TryParse(timeString.Substring(sIndex + 1, timeString.Length - sIndex2 - 1), out minutes);
+                        if(succeeded)
+                        {
+                            succeeded = Int32.TryParse(timeString.Substring(sIndex2 + 1), out seconds);
+                            if (succeeded)
+                                return (hours * 3600) + (minutes * 60) + seconds;
+                        }
+                    }
+                    return -1;
+                default:
+                    return -1;
+            };
+        }
+
+        private void appendTextBox(string message)
+        {
+            if (InvokeRequired)
+            {
+                this.BeginInvoke(new Action<string>(appendTextBox), new object[] { message });
+                return;
+            }
+
+            if (string.IsNullOrEmpty(message))
+                return;
+
+            CMDBox.Text += message + "\n";
+            CMDBox.SelectionStart = CMDBox.Text.Length;
+            CMDBox.ScrollToCaret();
+            if (message.Contains("[ffmpeg] Merging formats into ")) //Might be a bad way of doing this. :think:
+            {
+                int uIndex = message.LastIndexOf("\\");
+                //CMDBox.Text += "\n" + message.Substring(uIndex + 1, message.Length - (uIndex + 2)) + "\n";
+                DL_Vid_Name = message.Substring(uIndex + 1, message.Length - (uIndex + 2));
+                CMDBox.Text += "\n" + DL_Vid_Name + "\n\n";
+            }
+        }
+
+        private void changeStatusMessage(string message)
+        {
+            if (InvokeRequired)
+            {
+                this.BeginInvoke(new Action<string>(changeStatusMessage), new object[] { message });
+                return;
+            }
+            statusLabel.Text = message;
+        }
+
+        void cmd_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            appendTextBox(e.Data);
+        }
+
+        void cmd_Error(object sender, DataReceivedEventArgs e)
+        {
+            //Console.WriteLine("Error from other process");
+            //Console.WriteLine(e.Data);
+            //CMDBox.Text += "ERROR: " + e.Data;
+            //MessageBox.Show(e.Data);
+            appendTextBox(e.Data);
+        }
+
+        void runCMD(string arg)
+        {
+            ProcessStartInfo cmdStartInfo = new ProcessStartInfo();
+            cmdStartInfo.FileName = @"C:\Windows\System32\cmd.exe";
+            cmdStartInfo.RedirectStandardOutput = true;
+            cmdStartInfo.RedirectStandardError = true;
+            cmdStartInfo.RedirectStandardInput = true;
+            cmdStartInfo.UseShellExecute = false;
+            cmdStartInfo.CreateNoWindow = true;
+
+            Process cmdProcess = new Process();
+            cmdProcess.StartInfo = cmdStartInfo;
+            cmdProcess.ErrorDataReceived += cmd_Error;
+            cmdProcess.OutputDataReceived += cmd_DataReceived;
+            cmdProcess.EnableRaisingEvents = true;
+            cmdProcess.Start();
+            cmdProcess.BeginOutputReadLine();
+            cmdProcess.BeginErrorReadLine();
+
+            cmdProcess.StandardInput.WriteLine(arg);     //Execute ping bing.com
+            //cmdProcess.StandardInput.WriteLine("exit");                  //Execute exit.
+            cmdProcess.StandardInput.Flush();
+            cmdProcess.StandardInput.Close();
+            cmdProcess.WaitForExit();
+        }
+
+        void doAsyncExecute(int selection, string URL, int start, int length, string directory, string file_name)
+        {
+            changeStatusMessage("Downloading Youtube video");
+            runCMD("youtube-dl.exe --no-color --no-playlist --newline -o \"%cd%\\videos\\%(title)s-%(id)s.%(ext)s\" " + URL);
+            //if user wants it cut:
+            changeStatusMessage("Cutting video to specifications");
+            string commandBuilder = "ffmpeg.exe";
+
+            if(start > 0)
+                commandBuilder += " -ss " + start;
+            if (length > 0)
+                commandBuilder += " -t " + length;
+
+            commandBuilder += " -i \"%cd%\\videos\\" + DL_Vid_Name + "\"";
+
+            if(string.IsNullOrEmpty(directory))
+                commandBuilder += " \"%cd%\\cut-videos\\";
+            else
+                commandBuilder += " \"" + directory;
+
+            if(string.IsNullOrEmpty(file_name))
+                commandBuilder += DL_Vid_Name + "\"";
+            else 
+                commandBuilder += file_name + DL_Vid_Name.Substring(DL_Vid_Name.Length - 4) + "\"";
+
+            runCMD(commandBuilder);
+
+            //runCMD("ffmpeg.exe -ss 10 -t 60 -i \"%cd%\\videos\\" + DL_Vid_Name + "\" \"%cd%\\videos\\out1.mp4\"");
+            changeStatusMessage("Video cut to specifications");
+        }
+
+        private bool inRange(int number, int a, int b)
+        {
+            if (number >= a && number <= b)
+                return true;
+            else
+                return false;
+        }
+
+        private string checkFileName(string sFile)
+        {
+            int charVal;
+            if (sFile.Length > 150)
+                sFile = sFile.Substring(0, 150);
+
+            string sBuilder = "";
+            for (int i = 0; i < sFile.Length; i++)
+            {
+                charVal = (int)sFile[i];
+                if (inRange(charVal, 40, 41) || inRange(charVal, 43, 46) || inRange(charVal, 48, 57) || inRange(charVal, 65, 91)
+                    || charVal == 93 || inRange(charVal, 95, 123) || charVal == 125)
+                    sBuilder += sFile[i];
+                else
+                    sBuilder += "-";
+            }
+            return sBuilder;
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            var URL = linkBox.Text;
+            int start = readTime(startBox.Text);
+            int end = readTime(endBox.Text);
+            int length = readTime(lengthBox.Text);
+            var directory = directoryBox.Text + "\\";
+            //var file_name = nameBox.Text;
+            //perform check to see if file name is valid
+            string file_name = checkFileName(nameBox.Text);
+
+            CMDBox.Text = "";
+
+            if (start < 0 || end < 0 || length < 0)
+            {
+                MessageBox.Show("Start and End/Length need to be in seconds or HH:MM:SS");
+                return;
+            }
+
+            if (start >= end && start != 0 && end != 0)
+            {
+                MessageBox.Show("End time cannot be less than start time!");
+                return;
+            }
+
+            if (length == 0 && end > 0)
+            {
+                length = end - start;
+            }
+
+            //Perform check to see if directory is valid
+
+
+            //perform check to see if overwriting an existing file (or add an overwrite? checkbox..)
+
+            Thread thr = new Thread(()=>doAsyncExecute(1, URL, start, length, directory, file_name));
+            thr.Start();
+
+        }
+
+        private void fileButton_Click(object sender, EventArgs e)
+        {
+            //https://stackoverflow.com/questions/11624298/how-to-use-openfiledialog-to-select-a-folder
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    directoryBox.Text = fbd.SelectedPath;
+            }
+        }
+    }
+}
